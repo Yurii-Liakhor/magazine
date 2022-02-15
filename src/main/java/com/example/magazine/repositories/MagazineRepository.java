d237feeab7d24a9c84d1462c18c8ded2433be318
Yurii_Liakhor <lyakhoryura@gmail.com>
2022-02-15 17:02:17 +0200

    update 2

--- src/main/java/com/example/magazine/repositories/MagazineRepository.java
+++ src/main/java/com/example/magazine/repositories/MagazineRepository.java
@@ -1,16 +1,15 @@
 package com.example.magazine.repositories;
 
+import com.example.magazine.entity.Magazine;
 import com.example.magazine.entity.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
 import java.util.List;
-
 @Repository
 @Transactional
-public interface UserRepository extends CrudRepository<User, Long> {
-    List<User> findUserById(Long id);
-    Long removeUserById(Long id);
-    //todo add user
+public interface MagazineRepository extends CrudRepository<Magazine, Long> {
+    List<Magazine> findMagazineById(Long id);
+    Long removeMagazineById(Long id);
 }
